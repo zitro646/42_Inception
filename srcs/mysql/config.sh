fc2e0cf1f92bffdc352d57b859bf6f26c5c6a951
Zitro646 <mortizdelburgo@gmail.com>
2023-01-19 17:12:44 +0100

    wordpress cant connect to mariadb, beacuse of user creation problems - need to fix

--- srcs/mysql/config.sh
+++ srcs/mysql/config.sh
@@ -1,7 +1,7 @@
-echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE_NAME} ;"  >> /prueba.sql
-echo "CREATE USER IF NOT EXISTS ${MYSQL_ADMIN_USER} IDENTIFIED BY ${MYSQL_USER_PASSWORD};" >> /prueba.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_ADMIN_USER} IDENTIFIED BY ${MYSQL_USER_PASSWORD};" >> /prueba.sql
-echo "FLUSH PRIVILEGES;" >> /prueba.sql
-echo "ALTER USER root@localhost IDENTIFIED BY ${MYSQL_ROOT_PASSWORD};" >> /prueba.sql
-
+echo 'CREATE DATABASE IF NOT EXISTS '"$MYSQL_DATABASE_NAME" ';'  >> input_data/test.sql
+echo 'CREATE USER IF NOT EXISTS '"'$MYSQL_ADMIN_USER'"'@'"'%'"' IDENTIFIED BY '"'$MYSQL_USER_PASSWORD'" ';' >> input_data/test.sql
+echo 'GRANT ALL PRIVILEGES ON *.* TO '"$MYSQL_ADMIN_USER"' IDENTIFIED BY '"'$MYSQL_USER_PASSWORD'" ';' >> input_data/test.sql
+echo 'ALTER USER root@localhost IDENTIFIED BY '"'$MYSQL_ROOT_PASSWORD'" ';' >> input_data/test.sql
+#echo 'DROP USER IF EXISTS root;' >> input_data/test.sql
+echo 'FLUSH PRIVILEGES;' >> input_data/orders.sql
 #echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME ;" >> /prueba.sql
