5d7636bb39d7ab39156fc382e0f2239b6cf0e054
Zitro <mortizdelburgo@gmail.com>
2023-01-26 10:51:49 +0100

    aaaaaaaaaa

--- srcs/mysql/config.sh
+++ srcs/mysql/config.sh
@@ -1,8 +1,5 @@
-echo 'CREATE DATABASE IF NOT EXISTS '"$MYSQL_DATABASE_NAME" ';'  >> input_data/test.sql
-echo 'CREATE USER IF NOT EXISTS '"'$MYSQL_ADMIN_USER'"'@'"'%'"' IDENTIFIED BY '"'$MYSQL_USER_PASSWORD'" ';' >> input_data/test.sql
-#echo 'CREATE USER IF NOT EXISTS '"'$MYSQL_ADMIN_USER'"' IDENTIFIED BY '"'$MYSQL_USER_PASSWORD'" ';' >> input_data/test.sql
-echo 'GRANT ALL PRIVILEGES ON *.* TO '"'$MYSQL_ADMIN_USER'"'@'"'%'"' IDENTIFIED BY '"'$MYSQL_USER_PASSWORD'"' WITH GRANT OPTION;' >> input_data/test.sql
-echo 'ALTER USER root@localhost IDENTIFIED BY '"'$MYSQL_ROOT_PASSWORD'" ';' >> input_data/test.sql
-#echo 'DROP USER IF EXISTS root;' >> input_data/test.sql
-#echo 'FLUSH PRIVILEGES;' >> input_data/test.sql
-#echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME ;" >> /prueba.sql
+echo 'CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;'  >> database.sql
+echo "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" >> database.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}' WITH GRANT OPTION;" >> database.sql
+echo "ALTER USER root@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" >> database.sql
+#seed -i "s/127.0.0.1/0.0.0.0" /etc/mysql/mariadb.conf.d/50-server.cnf
--- srcs/wordpress/script.sh
+++ srcs/wordpress/script.sh
@@ -1,3 +1,9 @@
+
+wp core download --allow-root --path=var/www/html/wp_server
+wp config create --allow-root --dbname=${MYSQL_DATABASE_NAME} --dbuser=${MYSQL_ADMIN_USER} --dbpass=${MYSQL_USER_PASSWORD} --dbhost=mariadb --path=var/www/html/wp_server
+wp core install --allow-root --url=42-prueba.es --title="WP-CLI" --admin_user=${WORDPRESS_ADMIN_NAME} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=42_Inception@gmail.com --skip-email --path=var/www/html/wp_server
+#/etc/init.d/php7.3-fpm start
+#exec php-fpm7.3 -D
 while true; do
   sleep 100 
 done
