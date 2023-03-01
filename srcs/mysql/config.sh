7d0dc86bb77a4cef9f6e491fa6ee431c27371523
Zitro <mortizdelburgo@gmail.com>
2023-03-01 01:51:12 +0100

    the definitive cleaning
    ITS CLEAN, now i have to make it persistent :v

--- srcs/mysql/config.sh
+++ srcs/mysql/config.sh
@@ -1,5 +1,5 @@
-echo 'CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;'  >> database.sql
-echo "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" >> database.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}' WITH GRANT OPTION;" >> database.sql
-echo "ALTER USER root@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" >> database.sql
+echo 'CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE_NAME}' character set latin1 collate latin1_general_cs;'  >> database.sql 
+echo "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" >> database.sql 
+echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}' WITH GRANT OPTION;" >> database.sql 
+echo "ALTER USER root@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" >> database.sql 
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
--- srcs/wordpress/script.sh
+++ srcs/wordpress/script.sh
@@ -3,10 +3,11 @@
 #else
 #  echo "Directory does not exist"
 #fi
-#seed -i "s/listen = /run/php/php7.3-fpm.sock/listen = 0.0.0.0:9000/g" /etc/php/7.3/fpm/pool.d/www.conf
+#Modificamos el archivo www.conf para que escuche cualquier IP en ese puerto
+sed -i 's#listen = /run/php/php7.3-fpm.sock#listen = 0.0.0.0:9000#' /etc/php/7.3/fpm/pool.d/www.conf
+#Con esto descargamos y configuramos el server WP CLI
 wp core download --allow-root --path=var/www/html/wp_server
 wp config create --allow-root --dbname=${MYSQL_DATABASE_NAME} --dbuser=${MYSQL_ADMIN_USER} --dbpass=${MYSQL_USER_PASSWORD} --dbhost=mariadb --path=var/www/html/wp_server
-#chmod 644 /var/www/html/wp_server/wp-config.php
 wp core install --allow-root --url=mortiz-d.42.fr --title="WP CLI Demo" --admin_user=${WORDPRESS_ADMIN_NAME} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=42_Inception@gmail.com --skip-email --path=var/www/html/wp_server
 
 exec php-fpm7.3 -F
