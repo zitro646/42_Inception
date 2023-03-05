86af86324942b8d4148f3553639c230593bf10ac
Zitro <mortizdelburgo@gmail.com>
2023-03-05 18:38:02 +0100

    Added persistance after doing compose down / added domain name / added security to mysql

--- srcs/mysql/config.sh
+++ srcs/mysql/config.sh
@@ -1,5 +1,9 @@
+#Creamos una base de datos para el cliente y cambiamos los permisos para el root
 echo 'CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE_NAME}' character set latin1 collate latin1_general_cs;'  >> database.sql 
-echo "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" >> database.sql 
 echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}' WITH GRANT OPTION;" >> database.sql 
+echo "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" >> database.sql 
 echo "ALTER USER root@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" >> database.sql 
-sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+#Modificamos el archivo de configuracion de mysql para que escuche a cualquier peticion
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf 
+#Por seguridad añadida eliminamos las bases de datos imnecesarias
+echo 'DROP DATABASE IF EXISTS test;'  >> database.sql 
--- srcs/wordpress/script.sh
+++ srcs/wordpress/script.sh
@@ -1,13 +1,13 @@
-#if [ -d "/var/www/html/wp_server" ]; then
-#  echo "Directory exists"
-#else
-#  echo "Directory does not exist"
-#fi
+#Añadimos este directorio
+mkdir -p /run/php/
 #Modificamos el archivo www.conf para que escuche cualquier IP en ese puerto
-sed -i 's#listen = /run/php/php7.3-fpm.sock#listen = 0.0.0.0:9000#' /etc/php/7.3/fpm/pool.d/www.conf
-#Con esto descargamos y configuramos el server WP CLI
-wp core download --allow-root --path=var/www/html/wp_server
-wp config create --allow-root --dbname=${MYSQL_DATABASE_NAME} --dbuser=${MYSQL_ADMIN_USER} --dbpass=${MYSQL_USER_PASSWORD} --dbhost=mariadb --path=var/www/html/wp_server
-wp core install --allow-root --url=mortiz-d.42.fr --title="WP CLI Demo" --admin_user=${WORDPRESS_ADMIN_NAME} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=42_Inception@gmail.com --skip-email --path=var/www/html/wp_server
+sed -i "s#listen = /run/php/php7.3-fpm.sock#listen = 0.0.0.0:9000#" /etc/php/7.3/fpm/pool.d/www.conf
 
+#Con esto descargamos y configuramos el server WP CLI si se ejecuta por primera vez
+if [ ! -d "/var/www/html/wp_server" ]; then
+    wp core download --allow-root --path=var/www/html/wp_server
+    wp config create --allow-root --dbname=${MYSQL_DATABASE_NAME} --dbuser=${MYSQL_ADMIN_USER} --dbpass=${MYSQL_USER_PASSWORD} --dbhost=mariadb --path=var/www/html/wp_server
+    wp core install --allow-root --url=mortiz-d.42.fr --title="WP CLI Demo" --admin_user=${WORDPRESS_ADMIN_NAME} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=42_Inception@gmail.com --skip-email --path=var/www/html/wp_server
+fi
+#Arrancamos php-fpm en segundo plano 
 exec php-fpm7.3 -F
